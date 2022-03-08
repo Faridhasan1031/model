fafabd6f45ebfe9c1fd3aabb4b7d563670e44ed6
Faridhasan1031 <faridhasan523@gmail.com>
2022-03-08 23:24:44 +0600

    uplod

---
+++ fuctiom.php
@@ -0,0 +1,40 @@
+<?php
+
+$amount = 500;
+$type = 'dollar';
+
+
+switch($type){
+
+case 'dollar' :
+    $rate = 85.34;
+    break;
+
+
+    case 'pound' :
+        $rate = 116;
+        break;
+
+case 'euro' :
+    $rate = 95;
+    break;
+
+case 'won' :
+    $rate = 0.071;
+    break;
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+?>
